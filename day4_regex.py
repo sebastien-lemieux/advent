78e59cf7b7e7b9dc15f7d392099b95e70215d706
Jean-Philippe Laverdure <jp.laverdure@gmail.com>
2020-12-04 17:16:13 -0500

    Bask in the glory of MEGA REGEX

---
+++ day4_regex.py
@@ -0,0 +1,36 @@
+import re
+
+hits = 0
+passpt = ''
+validation_re = re.compile('^{} {}{} {} {} {} {} {}$'.format(
+    'byr:({})'.format('|'.join([str(i) for i in range(1920, 2003)])),
+    '(cid:[a-zA-Z0-9]+\s)?',
+    'ecl:({})'.format('|'.join(['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])),
+    'eyr:({})'.format('|'.join([str(i) for i in range(2020, 2031)])),
+    'hcl:(#[0-9a-f]{6})',
+    'hgt:(({})cm|({})in)'.format(
+        '|'.join([str(i) for i in range(150, 194)]),
+        '|'.join([str(i) for i in range(59, 77)])
+    ),
+    'iyr:({})'.format('|'.join([str(i) for i in range(2010, 2021)])),
+    'pid:[0-9]{9}'
+))
+
+with open('input_day4.txt') as input:
+    for line in input:
+        line = line.strip()
+        if line:
+            passpt += line + ' '
+        else:
+            passpt = ' '.join(sorted(passpt.split()))
+            if validation_re.match(passpt):
+                hits += 1
+
+            passpt = ''
+
+# pesky last line
+passpt = ' '.join(sorted(passpt.split()))
+if validation_re.match(passpt):
+    hits += 1
+
+print(hits)
