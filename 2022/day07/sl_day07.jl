572c5f6d73675b8d88845429a8ddc4f77776b305
Sebastien Lemieux <s.lemieux@umontreal.ca>
2022-12-07 19:38:22 -0500

    Soln 7

--- 2022/day07/sl_day07.jl
+++ 2022/day07/sl_day07.jl
@@ -1,58 +1,51 @@
-# struct Dir
-#     parent::Dir
-#     name::String
-#     sub::Vector{Dir}
-#     size::Int
-# end
-
 f = open("input.txt")
 
 pwd = Vector{String}()
 size = zeros(Int, 1)
 sizes = Vector{Int}() # saved sizes
-# the_sum = 0
+
+function cdpp()
+    push!(sizes, pop!(size))
+    pop!(pwd)
+    size[end] += sizes[end]
+end
+
 
 while !eof(f)
     line = split(readline(f), " ")
-    println(line)
+    # println(line)
     if line[1] == "\$"
         if line[2] == "cd"
             if line[3][1] == '/'
                 empty!(pwd)
             elseif line[3] == ".."
-                push!(sizes, pop!(size))
-                # For part 1
-                # if sizes[end] <= 100000
-                #     println("Got: $(pwd)")
-                #     the_sum += sizes[end]
-                # end
-                pop!(pwd)
-                size[end] += sizes[end]
+                cdpp()
             else
                 push!(pwd, line[3])
                 push!(size, 0)
             end
-            println("/" * join(pwd, "/"))
-        elseif line[2] == "ls"
-            # skip
+            # println("/" * join(pwd, "/"))
         end
     elseif line[1] == "dir"
         # do something with it!
     else
         size[end] += parse(Int, line[1])
     end
-    println(size)
+    # println(size)
 end
 
 while !isempty(pwd)
-    push!(sizes, pop!(size))
-    # For part 1
-    # if sizes[end] <= 100000
-    #     println("Got: $(pwd)")
-    #     the_sum += sizes[end]
-    # end
-    pop!(pwd)
-    size[end] += sizes[end]
+    cdpp()
 end
 
-# the_sum
+looking_for = size[end] - 40_000_000
+
+for s in sort(sizes)
+    if s >= looking_for
+        println(s)
+        break
+    end
+end
+
+# Part 1: 1432936
+# Part 2: 272298
