6947f303469319de2e5dc6fea86408a45860b46a
Sebastien Lemieux <s.lemieux@umontreal.ca>
2022-12-11 17:12:50 -0500

    Soln 9, part 2.

--- 2022/day09/sl_day09.jl
+++ 2022/day09/sl_day09.jl
@@ -12,33 +12,37 @@ Base.sign(a::Pos) = Pos(sign(a.x), sign(a.y))
 diag_dist(p::Pos) = max(abs(p.x), abs(p.y))
 dir = Dict("U" => Pos(0, 1), "D" => Pos(0, -1), "L" => Pos(-1, 0), "R" => Pos(1, 0))
 
-head = Pos(0, 0)
-tail = Pos(0, 0)
-record = Set{Pos}()
-push!(record, Pos(0, 0))
+## Part 2 (part 1 in previous commit)
 
-function adjust_once(h, t)
+rope = fill(Pos(0, 0), 10)
+
+record = Set{Pos}()
+push!(record, rope[10])
+
+function adjust_once(h::Pos, t::Pos, push::Bool)
     new_t = t + sign(h - t)
-    # new_t == head && return t
-    push!(record, new_t)
+    push && push!(record, new_t)
     return new_t
 end
 
 
 readFile("input.txt") do line
-    global head, tail
+    global rope
     tmp = split(line)
     n = parse(Int, tmp[2])
     d = dir[tmp[1]]
-    println(d, n)
-    head += d * n
-    while diag_dist(head - tail) > 1
-        tail = adjust_once(head, tail)
+    # println(d, n)
+    rope[1] += d * n
+    done = false
+    while !done
+        done = true
+        for i=2:10
+            if diag_dist(rope[i-1] - rope[i]) > 1
+                rope[i] = adjust_once(rope[i-1], rope[i], i == 10)
+                done = false
+            end
+        end
     end
-end; # 6313 too low
+end;
 
-a = Pos(1,1)
-f!(b) = (b *= 2)
-a
-f!(a)
-a
+record # 2504
