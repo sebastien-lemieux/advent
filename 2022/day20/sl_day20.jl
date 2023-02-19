b240160a089794d9b810d039ad8800776140232c
Sebastien Lemieux <s.lemieux@umontreal.ca>
2023-02-19 18:06:35 -0500

    Sol to day 20 part 1.

---
+++ 2022/day20/sl_day20.jl
@@ -0,0 +1,85 @@
+include("../../sl_common.jl")
+
+abstract type LinkedList{T} end
+
+mutable struct Nil{T} <: LinkedList{T}
+end
+
+mutable struct Node{T} <: LinkedList{T}
+    el::T
+    prev::LinkedList{T}
+    next::LinkedList{T}
+end
+
+# a = Node(23, Nil{Int}(), Nil{Int}())
+# b = Node(24, a, Nil{Int}())
+
+function circular(v)
+    t = eltype(v)
+    vec = Vector{LinkedList{t}}()
+    first = Node(v[1], Nil{t}(), Nil{t}())
+    push!(vec, first)
+    last = first
+    for i=2:length(v)
+        last.next = Node(v[i], last, Nil{t}())
+        last = last.next
+        push!(vec, last)
+    end
+    first.prev = last
+    last.next = first
+    return first, vec
+end
+
+function Base.show(io::IO, l::Node{T}) where T
+    first = l
+    print(io, "$(l.el)")
+    l = l.next
+    while l != first
+        print(io, ", $(l.el)")
+        l = l.next
+    end
+end
+
+function move(x::Node{Int}) ## deal with neg!
+    x.prev.next = x.next
+    x.next.prev = x.prev
+    cur = x.next
+    for i=1:abs(x.el)
+        cur = (x.el > 0) ? cur.next : cur.prev
+    end
+    x.next = cur
+    x.prev = cur.prev
+    x.next.prev = x
+    x.prev.next = x
+end
+
+a, v = circular([1, 2, -3, 3, -2, 0, 4]);
+
+for i in v
+    move(i)
+end
+
+a
+
+f = readFile("input.txt", Vector{Int}) do line
+    parse(Int, line)
+end
+
+n = length(f)
+a, v = circular(f);
+for i in v
+    move(i)
+end
+
+while a.el != 0
+    a = a.next
+end
+
+sum = 0
+for it = 1:3
+    for i=1:1000
+        a = a.next
+    end
+    sum += a.el
+end
+sum ## Part 1: 2622
