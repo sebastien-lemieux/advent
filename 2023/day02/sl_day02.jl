f69795d850d37252de62cb771173e04b782870dc
Sebastien Lemieux <s.lemieux@umontreal.ca>
2023-12-02 21:50:12 -0500

    Day 2, part 1

---
+++ 2023/day02/sl_day02.jl
@@ -0,0 +1,31 @@
+include("../../sl_common.jl")
+
+gameId(line) = parse(Int, split(line)[2])
+parseGame(line) = split(line, ";")
+parsePull(line) = split(line, ",")
+function parseCount(line)
+    l = split(line)
+    return parse(Int, l[1]), l[2]
+end
+testCount(c, d) = d[c[2]] < c[1]
+
+d = Dict(
+    "red" => 12,
+    "blue" => 14,
+    "green" => 13
+)
+
+theSum = 0
+
+possible = readFile("input.txt") do line
+    println(line)
+    a, b = split(line, ":")
+    id = gameId(a)
+
+    for p in parsePull.(parseGame(b))
+        println(any([testCount(c, d) for c in parseCount.(p)]))
+    end
+
+    any([any([testCount(c, d) for c in parseCount.(p)])
+         for p in parsePull.(parseGame(b))]) ? 0 : id
+end
