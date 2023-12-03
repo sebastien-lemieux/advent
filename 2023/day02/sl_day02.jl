684a562efae65db82914e64fcb6b98a7781d9e1f
Sebastien Lemieux <s.lemieux@umontreal.ca>
2023-12-02 22:07:48 -0500

    Day 2 solutions

--- 2023/day02/sl_day02.jl
+++ 2023/day02/sl_day02.jl
@@ -9,6 +9,8 @@ function parseCount(line)
 end
 testCount(c, d) = d[c[2]] < c[1]
 
+## part 1
+
 d = Dict(
     "red" => 12,
     "blue" => 14,
@@ -29,3 +31,24 @@ possible = readFile("input.txt") do line
     any([any([testCount(c, d) for c in parseCount.(p)])
          for p in parsePull.(parseGame(b))]) ? 0 : id
 end
+
+
+## part 2
+
+readFile("input.txt") do line
+    println(line)
+    a, b = split(line, ":")
+    id = gameId(a)
+
+    d = Dict(
+        "red" => 0,
+        "blue" => 0,
+        "green" => 0)
+
+    for p in parsePull.(parseGame(b))
+        for c in parseCount.(p)
+            d[c[2]] = max(d[c[2]], c[1])
+        end
+    end
+    d |> values |> prod
+end |> sum
