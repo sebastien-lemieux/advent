da54f55acfd485657bb13595816e8841143bfe78
Sebastien Lemieux <s.lemieux@umontreal.ca>
2023-12-06 22:54:17 -0500

    Solution day 6, part 2

--- 2023/day06/sl_day06.jl
+++ 2023/day06/sl_day06.jl
@@ -10,5 +10,14 @@ function quadRoots(T, D)
     return floor(Int, compute_root(-1)) - ceil(Int, compute_root(1)) + 1
 end
 
-
 quadRoots.(df.times, df.dist) |> prod
+
+
+## part 2
+
+df = open("input.txt") do f
+    read() = parse.(Int, join(split(split(readline(f), ":")[2])))
+    DataFrame(times=read(), dist=read())
+end
+
+quadRoots.(df.times, df.dist)
