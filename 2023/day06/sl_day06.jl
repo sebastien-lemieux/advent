8006fee2c4880a8dc080b820bd870586d85b3aea
Sebastien Lemieux <s.lemieux@umontreal.ca>
2023-12-06 22:51:36 -0500

    Solution 6, part 1

---
+++ 2023/day06/sl_day06.jl
@@ -0,0 +1,14 @@
+using DataFrames
+
+df = open("input.txt") do f
+    read() = parse.(Int, split(split(readline(f), ":")[2]))
+    DataFrame(times=read(), dist=read())
+end
+
+function quadRoots(T, D)
+    compute_root(sign) = (-T + sign * sqrt(T^2 - 4D)) / -2
+    return floor(Int, compute_root(-1)) - ceil(Int, compute_root(1)) + 1
+end
+
+
+quadRoots.(df.times, df.dist) |> prod
