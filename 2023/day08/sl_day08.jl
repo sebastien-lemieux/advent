bdc532cdaec1b2834a36297c61792a2ba8151e6c
Sebastien Lemieux <s.lemieux@umontreal.ca>
2023-12-09 14:19:22 -0500

    Solution day 8, part 1

--- 2023/day08/sl_day08.jl
+++ 2023/day08/sl_day08.jl
@@ -1,2 +1,25 @@
 include("../../sl_common.jl")
 
+inst, registry = open("input.txt") do f
+    registry = Dict{String, Tuple{String, String}}()
+
+    inst = readline(f)
+    readline(f)
+    while !eof(f)
+        line = readline(f)
+
+        registry[line[1:3]] = (line[8:10], line[13:15])
+    end
+    return inst, registry
+end
+
+cur = "AAA"
+i = 0
+while cur != "ZZZ"
+    d = inst[i % length(inst) + 1]
+    println("$cur : $d -> $(registry[cur])")
+    cur = registry[cur][(d == 'L') ? 1 : 2]
+    i += 1
+end
+i
+
