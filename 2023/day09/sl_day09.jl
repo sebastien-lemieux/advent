e1546d777a2c0095410d35776385aa1988b924f5
Sebastien Lemieux <s.lemieux@umontreal.ca>
2023-12-14 18:24:28 -0500

    Solution day 9, part 2

--- 2023/day09/sl_day09.jl
+++ 2023/day09/sl_day09.jl
@@ -14,19 +14,27 @@ end
 function find_last(layer::T) where T <: AbstractArray
     println("lets go: $layer")
     next, all_zero = next_layer(layer)
-    println(next)
     all_zero && return last(layer)
     l = find_last(next)
     println("$l layer: $(last(layer)) next: $(last(next))")
     last(layer) + l
 end
 
+function find_first(layer::T) where T <: AbstractArray
+    next, all_zero = next_layer(layer)
+    println("Lets go: $layer : $next")
+    all_zero && return first(layer)
+    l = find_first(next)
+    println("$l layer: $(first(layer)) next: $(first(next))")
+    first(layer) - l
+end
+
+
 hists = readFile("input.txt") do line
     z = parse.(Int, split(line))
-    return find_last(z)
+    return find_first(z)
 end |> sum
 
-next_layer(hists[1])
-
-find_last([1, 3, 6, 10, 15, 21])
-
+find_first([10, 13, 16, 21, 30, 45])
+find_first([1, 3, 6, 10, 15, 21, 28])
+find_first([0, 3, 6, 9, 12, 15])
