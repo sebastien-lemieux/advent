b34f6ec376f40ee9c519a1ccc2c00d74c7eda422
Sebastien Lemieux <s.lemieux@umontreal.ca>
2023-12-16 22:31:20 -0500

    Solution day 12, part 2 (@memoize)

--- 2023/day12/sl_day12.jl
+++ 2023/day12/sl_day12.jl
@@ -2,11 +2,11 @@ include("../../sl_common.jl")
 
 rec_v = readFile("input.txt", Vector{Tuple{String, Vector{Int}}}) do line
     rec, brok = split(line)
-    return rec, parse.(Int, split(brok, ","))
+    rec, parse.(Int, split(brok, ","))
 end
 
-function bt(rec, pos, len_v)
-    # println("$rec[$pos]: $len_v")
+function bt(rec, len_v)
+    # println("$rec: $len_v")
     if isempty(len_v)
         isempty(rec) && return 1
         any(occursin.(collect(rec), "#")) && return 0
@@ -17,12 +17,43 @@ function bt(rec, pos, len_v)
     nb = 0
     if length(rec) >= flv && all(occursin.(collect(rec[1:flv]), "#?"))
         if  length(rec) == flv || rec[flv+1] in "?."
-            nb += bt(rec[flv+2:end], flv+2, len_v[2:end])
+            nb += bt(rec[flv+2:end], len_v[2:end])
         end
     end
-    rec[1] in ".?" && (nb += bt(rec[2:end], pos+1, len_v))
+    rec[1] in ".?" && (nb += bt(rec[2:end], len_v))
     return nb
 end
 
-[bt(rec, 1, len_v) for (rec, len_v) in rec_v] |> sum
+[bt(rec, len_v) for (rec, len_v) in rec_v] |> sum
+
+## Part 2
+
+unfold(rec, len_v) = join(repeat([rec], 5), "?"), repeat(len_v, 5)
+
+include("test_macro.jl")
+@memoize(mem, function bt(rec, len_v)
+    # println("$rec: $len_v")
+    if isempty(len_v)
+        isempty(rec) && return 1
+        any(occursin.(collect(rec), "#")) && return 0
+        return 1
+    end
+    isempty(rec) && return 0
+    flv = first(len_v)
+    nb = 0
+    if length(rec) >= flv && all(occursin.(collect(rec[1:flv]), "#?"))
+        if  length(rec) == flv || rec[flv+1] in "?."
+            nb += bt(rec[flv+2:end], len_v[2:end])
+        end
+    end
+    rec[1] in ".?" && (nb += bt(rec[2:end], len_v))
+    return nb
+end
+)
+
+
+
+mem = Dict()
+
+[bt(unfold(rec, len_v)...) for (rec, len_v) in rec_v] |> sum
 
---
+++ 2023/day12/test_macro.jl
@@ -0,0 +1,66 @@
+
+macro memoize(mem_symbol, def)
+    func_args = nothing
+    func_start = false
+    function memoize_aux(expr)
+        new_expr = expr
+        # println("BEFORE:  $expr ||| ")
+        if isa(expr, Expr)
+            # println("head: $(expr.head), args: $(expr.args)")
+            if expr.head == :block && !func_start
+                func_start = true
+                args = Any[:(haskey($mem_symbol, $func_args) && return $mem_symbol[$func_args])]
+                for item in expr.args
+                    push!(args, memoize_aux(item))
+                end
+                new_expr = Expr(:block, args...)
+            elseif expr.head == :call # Assumes the first call carries the arguments for the key
+                # println("***********CALL: $(expr.args[2:end])")
+                isnothing(func_args) && (func_args = Expr(:tuple, expr.args[2:end]...)) ## capture des arguments
+                # println(func_args)
+            elseif expr.head == :return
+                # println("***********got it!!!  $(expr.args)")
+                new_expr = Expr(:block, 
+                            Expr(:global, :($mem_symbol)),
+                            Expr(:(=), :tmp_res, expr.args...),
+                            Expr(:call, :setindex!, :($mem_symbol), :tmp_res, func_args),
+                            Expr(:return, :tmp_res))
+            else
+                args = []
+                for item in expr.args
+                    push!(args, memoize_aux(item))
+                end
+                new_expr = Expr(expr.head, args...)
+            end
+        end
+        # println("AFTER:  $new_expr")
+        # println()
+        return new_expr
+    end
+
+    expr = memoize_aux(def)
+    return eval(expr)
+end
+
+# mem = Dict()
+
+# @memoize(mem, fact(n) = if n == 1
+#     return 1
+# else
+#     println("compute $n")
+#     return n * fact(n-1)
+# end)
+
+# fact(6)
+
+# @memoize(mem, function fib(n)
+#     if n < 3
+#         return 1
+#     else
+#         println("compute $n")
+#         return fib(n-1) + fib(n-2)
+#     end
+# end)
+# mem = Dict()
+
+# fib(6)
