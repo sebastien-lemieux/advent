a200f287df8220536f3ed1009211f596e6b62270
Sebastien Lemieux <s.lemieux@umontreal.ca>
2023-12-22 10:09:21 -0500

    Solution day 15, part 1

--- 2023/day15/sl_day15.jl
+++ 2023/day15/sl_day15.jl
@@ -1,4 +1,9 @@
-include("../../sl_common.jl")
+# include("../../sl_common.jl")
 
+v = split(readline("input.txt"), ",")
 
+aHash(str) = reduce((h, c) -> 17 * (h + Int(c)) % 256, str; init=0)
 
+aHash("HASH")
+
+[aHash(str) for str in v] |> sum
