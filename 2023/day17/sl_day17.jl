e5f61913250f21db05eafc29cd8dd6457209ff23
Sebastien Lemieux <s.lemieux@umontreal.ca>
2023-12-22 20:46:24 -0500

    Solution day 17, part 2

--- 2023/day17/sl_day17.jl
+++ 2023/day17/sl_day17.jl
@@ -1,9 +1,5 @@
 include("../../sl_common.jl")
 
-m = readGrid("test.txt") do c
-    parse(Int, c)
-end
-
 using DataStructures
 
 struct Path
@@ -12,66 +8,101 @@ struct Path
     fwd_left::Int
     heat::Int
 end
-Path(p, d) = Path(p, d, 3, 0)
-# left_turn(p::Path) = Path(p.path, turn_left(p.dir), p.fwd_left, p.heat)
-# right_turn(p::Path) = Path(p.path, turn_right(p.dir), p.fwd_left, p.heat)
+Path(p, d) = Path(p, d, 10, 0)
 DataStructures.enqueue!(q::PriorityQueue{Path, T}, p::Path) where T <: Number = enqueue!(q, p, p.heat)
 
 function update_heat(g, p)
     cur = last(p.path)
-    iswithin(cur, size(g)) && p.fwd_left ≥ 1 && return [Path(p.path, p.dir, p.fwd_left, p.heat + g[cur])]
-    return Path[]
+    prev = p.path[end-1]
+    if iswithin(cur, size(g)) && (p.fwd_left ≥ 0)
+        cur == prev && return [Path(p.path, p.dir, p.fwd_left, p.heat)]
+        return [Path(p.path, p.dir, p.fwd_left, p.heat + g[cur])]
+    else
+        return Path[]
+    end
 end
 
-move(p::Path, dir) = Path(vcat(p.path, last(p.path) + dir), dir, (dir == p.dir) ? (p.fwd_left - 1) : 3, p.heat)
+move(p::Path, dir) = ((dir == p.dir) ? Path(vcat(p.path, last(p.path) + dir), dir, p.fwd_left - 1, p.heat) :
+                                       Path(vcat(p.path, last(p.path)), dir, 10, p.heat))
 move(p::Path) = move(p, p.dir)
 
-nrow, ncol = size(m)
-best = PriorityQueue{Path, Real}()
+function search(m)
+    nrow, ncol = size(m)
+    best = PriorityQueue{Path, Real}()
 
-enqueue!(best, Path([Pos(1, 1)], right))
-enqueue!(best, Path([Pos(1, 1)], down))
+    enqueue!(best, Path([Pos(1, 1)], right))
+    enqueue!(best, Path([Pos(1, 1)], down))
 
-mem = Dict{Tuple{Pos, Pos, Int}, Path}()
+    mem = Dict{Tuple{Pos, Pos, Int}, Path}()
 
-while !isempty(best)
-    cur = dequeue!(best)
+    while !isempty(best)
+        cur = dequeue!(best)
 
-    # l = [move(cur),
-    #      move(cur, left_turn(cur.dir)) |> move,
-    #      move(cur, right_turn(cur.dir)) |> move]
+        l = [move(cur)]
+        (cur.fwd_left ≤ 6) && (last(cur.path) != Pos(nrow, ncol)) && push!(l, move(cur, left_turn(cur.dir)))
+        (cur.fwd_left ≤ 6) && (last(cur.path) != Pos(nrow, ncol)) && push!(l, move(cur, right_turn(cur.dir)))
 
-    l = [move(cur),
-    move(cur, left_turn(cur.dir)),
-    move(cur, right_turn(cur.dir))]
+        for p in (reduce(l; init=Path[]) do ch, p
+                return vcat(ch, update_heat(m, p))
+            end)
 
-    for p in (reduce(l; init=Path[]) do ch, p
-            return vcat(ch, update_heat(m, p))
-        end)
-        key = (last(p.path), p.dir, p.fwd_left)
-        cont = false
-        if key ∈ keys(mem)
-            mem[key].heat > p.heat && (mem[key] = p; cont = true)
-        else
-            mem[key] = p
-            cont = true
+            key = (last(p.path), p.dir, p.fwd_left)
+            
+            (last(p.path) == Pos(nrow, ncol)) && (p.fwd_left > 6) && continue
+            
+            cont = false
+            if key ∈ keys(mem)
+                (mem[key].heat > p.heat) && (mem[key] = p; cont = true)
+            else
+                mem[key] = p
+                cont = true
+            end
+
+            cont && last(p.path) != Pos(nrow, ncol) && enqueue!(best, p)
         end
-
-        cont && last(p.path) != Pos(nrow, ncol) && enqueue!(best, p)
     end
 
+    theBest = nothing
+    for k ∈ keys(mem)
+        k[1] != Pos(nrow, ncol) && continue
+        if theBest === nothing
+            theBest = mem[k]
+        else
+            mem[k].heat < theBest.heat && (theBest = mem[k])
+        end
+    end
+    theBest, mem
 end
 
-theBest = nothing
-for k ∈ keys(mem)
-    k[1] != Pos(nrow, ncol) && continue
-    println("$k -> $(mem[k].heat)")
-    if theBest === nothing
-        theBest = mem[k]
-    else
-        mem[k].heat < theBest.heat && (theBest = mem[k])
+function display(g, p::Path)
+    nrow, ncol = size(m)
+    for i=1:nrow
+        for j=1:ncol
+            if Pos(i, j) ∈ p.path
+                print('.')
+            else
+                print(g[i, j])
+            end
+        end
+        println()
     end
 end
+
+m = readGrid("input.txt") do c
+    parse(Int, c)
+end
+
+theBest, mem = search(m)
 theBest.heat
+display(m, theBest)
+
+## 1006: too low
+## 1010: finalement!
+## 1023: wrong
+## 1028: wrong
+## 1035: wrong
+## 1063: too high
+## 1064: too high
+
 
 
