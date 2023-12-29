7de3a3cf8f7e2f63cce953eb396529ff46b8ab77
Sebastien Lemieux <s.lemieux@umontreal.ca>
2023-12-29 18:49:23 -0500

    Update to common

--- sl_common.jl
+++ sl_common.jl
@@ -108,3 +108,49 @@ Base.getindex(idx::StrIndex, v::AbstractVector{String}) = [idx[s] for s in v]
 Base.getindex(idx::StrIndex, v::AbstractVector{<:Integer}) = [idx[i] for i in v]
 Base.getindex(idx::StrIndex, v::AbstractVector{<:Bool}) = idx.id2str[v]
 Base.length(idx::StrIndex) = length(idx.id2str)
+
+## Memoization ------------------------
+
+macro memoize(mem_symbol, def)
+    func_args = nothing
+    func_start = false
+    function memoize_aux(expr)
+        new_expr = expr
+        # println("BEFORE:  $expr ||| ")
+        if isa(expr, Expr)
+            # println("head: $(expr.head), args: $(expr.args)")
+            if expr.head == :block && !func_start
+                func_start = true
+                args = Any[:(haskey($mem_symbol, $func_args) && return $mem_symbol[$func_args])]
+                for item in expr.args
+                    push!(args, memoize_aux(item))
+                end
+                new_expr = Expr(:block, args...)
+            elseif expr.head == :call # Assumes the first call carries the arguments for the key
+                # println("***********CALL: $(expr.args[2:end])")
+                isnothing(func_args) && (func_args = Expr(:tuple, expr.args[2:end]...)) ## capture des arguments
+                # println(func_args)
+            elseif expr.head == :return
+                # println("***********got it!!!  $(expr.args)")
+                new_expr = Expr(:block, 
+                            Expr(:global, :($mem_symbol)),
+                            Expr(:(=), :tmp_res, expr.args...),
+                            Expr(:call, :setindex!, :($mem_symbol), :tmp_res, func_args),
+                            Expr(:return, :tmp_res))
+            else
+                args = []
+                for item in expr.args
+                    push!(args, memoize_aux(item))
+                end
+                new_expr = Expr(expr.head, args...)
+            end
+        end
+        # println("AFTER:  $new_expr")
+        # println()
+        return new_expr
+    end
+
+    expr = memoize_aux(def)
+    println(expr) ## Just to verify...
+    return eval(expr)
+end
